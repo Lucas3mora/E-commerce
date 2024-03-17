638076b02b5ebce4aa0c047185e5a46aa5d4f51a
amr <amrahmedhelal198@gmail.com>
2024-03-17 15:34:44 +0200

    Added Shopping Cart by using offcanvas and Added Cart item file

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,52 @@
+import { Button, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import storeItems from "../data/items.json"
+import { formatCurrency } from "../utilities/formatCurrency"
+
+type CartItemProps = {
+  id: number
+  quantity: number
+}
+
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart()
+  const item = storeItems.find(i => i.id === id)
+  if (item == null) return null
+
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center fst-italic">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "175px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted fst-italic fw-bold" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted fst-italic fw-bold" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+      <Button
+        
+        variant="outline-primary"
+        className="rounded-circle" 
+        
+        
+        onClick={() => removeFromCart(item.id)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16" $hover-bg="dark">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
+        &times;
+      </Button>
+    </Stack>
+  )
+}
